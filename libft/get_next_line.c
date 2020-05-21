946259b484fb335392b4a7c6f3b64adc4a16a03b
morganjander <19morgan89@gmail.com>
2020-05-21 12:04:45 +0200

    fixed gnl

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -10,105 +10,58 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "get_next_line.h"
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <stdio.h>
 
-/*
-** returns the correct node in the list of files,
-** using content_size as an identifier. Creates a
-** new node with an empty string if one with the
-** correct fd does not exist
-*/
-
-static t_list	*getfile(t_list **files, int fd)
-{
-	t_list	*tmp;
-
-	tmp = *files;
-	while (tmp)
-	{
-		if ((int)tmp->content_size == fd)
-			return (tmp);
-		tmp = tmp->next;
-	}
-	tmp = ft_lstnew("\0", fd);
-	ft_lstadd(files, tmp);
-	tmp = *files;
-	return (tmp);
-}
-
-/*
-** reads from the fd into the buffer, then assigns
-** the content of buffer into the saved string,
-** until the saved string contains a newline
-** character.
-*/
-
-void			read_until(int fd, char **saved)
-{
-	char	buf[BUFF_SIZE + 2];
-	char	*tmp;
-	int		ret;
-
-	while (!(ft_strchr(*saved, '\n')))
-	{
-		ret = read(fd, buf, BUFF_SIZE);
-		buf[ret] = (ret < BUFF_SIZE) ? 3 : 0;
-		buf[ret + 1] = 0;
-		tmp = ft_strdup(*saved);
-		free(*saved);
-		*saved = ft_strjoin(tmp, buf);
-		free(tmp);
-		if (!ret)
-			return ;
-	}
-}
-
-/*
-** checks if the 1st char of the saved sting is the eof character
-** (reached the end of the file, so returns 0)
-** assigns a ptr to the newline char in the saved string,
-** replaces it with null, copies the string up until null
-** into the line, then frees string and copies whatever
-** is after null (newline) back into the string.
-*/
-
-int				ft_getline(char **saved, char **line)
+int		ft_new_line(char **s, char **line, int fd, int ret)
 {
 	char	*tmp;
-	char	*ptr;
-	char	*eof;
+	int		len;
 
-	if ((*saved)[0] == 3)
-		return (0);
-	if ((eof = ft_strchr(*saved, 3)))
-		if (!(ft_strchr(*saved, '\n')))
-			*eof = '\n';
-	tmp = ft_strdup(*saved);
-	ptr = ft_strchr(tmp, '\n');
-	*ptr = 0;
-	*line = ft_strdup(tmp);
-	free(*saved);
-	*saved = ft_strdup(ptr + 1);
-	free(tmp);
+	len = 0;
+	while (s[fd][len] != '\n' && s[fd][len] != '\0')
+		len++;
+	if (s[fd][len] == '\n')
+	{
+		*line = ft_strsub(s[fd], 0, len);
+		tmp = ft_strdup(s[fd] + len + 1);
+		free(s[fd]);
+		s[fd] = tmp;
+		if (s[fd][0] == '\0')
+			ft_strdel(&s[fd]);
+	}
+	else if (s[fd][len] == '\0')
+	{
+		if (ret == BUFF_SIZE)
+			return (get_next_line(fd, line));
+		*line = ft_strdup(s[fd]);
+		ft_strdel(&s[fd]);
+	}
 	return (1);
 }
 
-int				get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
-	static t_list	*files;
-	t_list			*curr;
+	static char	*s[255];
+	char		buf[BUFF_SIZE + 1];
+	char		*tmp;
+	int			ret;
 
-	if (fd < 0 || !line || (read(fd, 0, 0) < 0))
+	if (fd < 0 || line == NULL)
 		return (-1);
-	curr = getfile(&files, fd);
-	read_until(fd, (char **)&curr->content);
-	return (ft_getline((char **)&curr->content, line));
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		if (s[fd] == NULL)
+			s[fd] = ft_strnew(1);
+		tmp = ft_strjoin(s[fd], buf);
+		free(s[fd]);
+		s[fd] = tmp;
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0 && (s[fd] == NULL || s[fd][0] == '\0'))
+		return (0);
+	return (ft_new_line(s, line, fd, ret));
 }
--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -12,10 +12,16 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFF_SIZE 1
+
+# include "fcntl.h"
+# include "sys/types.h"
+# include "sys/uio.h"
+# include "unistd.h"
+# include "stdlib.h"
 # include "libft.h"
 
-# define BUFF_SIZE 4
-
-int get_next_line(const int fd, char **line);
+int		ft_new_line(char **s, char **line, int fd, int ret);
+int		get_next_line(const int fd, char **line);
 
 #endif
